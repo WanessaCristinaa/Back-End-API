e7ecba43bbae7e0b71053953fdc019b74fd9c152
WanessaCristinaa <wanessacristina158@gmail.com>
2024-01-10 11:23:27 -0300

    banco de dados log

--- bancodeDados.js
+++ bancodeDados.js
@@ -4,4 +4,6 @@ async function conectaBancodeDados(){
     console.log('Conexão com o banco de dados iniciou')
 
     await mongoose.connect('mongodb+srv://wanessacristina158:Y9SCbYuMtH86IpmW@clustermulheres.1q4jamj.mongodb.net/?retryWrites=true&w=majority')
+    
+    console.log('Conexão com o banco de dados feita com sucesso!')
 }
