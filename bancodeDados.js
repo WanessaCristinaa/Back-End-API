f47ff1774af072f5480075a57cf733123239dad2
WanessaCristinaa <wanessacristina158@gmail.com>
2024-01-10 11:27:16 -0300

    adicionando try e removendo node yml

--- bancodeDados.js
+++ bancodeDados.js
@@ -1,9 +1,14 @@
 const mongoose = require('mongoose')
 
 async function conectaBancodeDados(){
+    try {
     console.log('Conexão com o banco de dados iniciou')
 
     await mongoose.connect('mongodb+srv://wanessacristina158:Y9SCbYuMtH86IpmW@clustermulheres.1q4jamj.mongodb.net/?retryWrites=true&w=majority')
     
     console.log('Conexão com o banco de dados feita com sucesso!')
+     
+    }catch(erro){
+        console.log(erro)
+    }
 }
