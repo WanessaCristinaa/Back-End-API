40c24afddd8a3beb771b42d65cfe0742b0d66e21
WanessaCristinaa <wanessacristina158@gmail.com>
2024-01-10 10:57:25 -0300

    adicionando mongoose e criando arquivo

---
+++ bancodeDados.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose')
+
+async function conectaBancodeDados(){
+    console.log('Conexão com o banco de dados iniciou')
+
+    await mongoose.connect('mongodb+srv://wanessacristina158:Y9SCbYuMtH86IpmW@clustermulheres.1q4jamj.mongodb.net/?retryWrites=true&w=majority')
+}
