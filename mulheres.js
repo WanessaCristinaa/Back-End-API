387a5bbeed8b2e59c817ad127bd3c992adeb993d
WanessaCristinaa <wanessacristina158@gmail.com>
2024-01-13 11:52:31 -0300

    Delete

--- mulheres.js
+++ mulheres.js
@@ -87,16 +87,16 @@ async function corrigeMulher(request, response){
 }
 
 //DELETE
-function deletaMulher(request, response){
-  function todasMenosEla(mulher) {
-    if(mulher.id !== request.params.id){
-        return mulher
-    }
+async function deletaMulher(request, response){
+  
+  try{
+    await Mulher.findByIdAndDelete(request.params.id)
+    response.json({ mensagem: 'Mensagem deletada com sucesso!' })
+
+  } catch (erro) {
+      console.log(erro)
   }
 
-  const mulheresQueFicam = mulheres.filter(todasMenosEla)
-
-  response.json(mulheresQueFicam)
 }
 
 
